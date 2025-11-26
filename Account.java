55fdc86a289bb95b1364ff42d4c32bbccaab808b
dkdp21 <divyanshudeeppandey97981312@gmail.com>
2025-11-26 14:09:36 +0530

    Create Account.java

---
+++ Account.java
@@ -0,0 +1,59 @@
+
+public class Account {
+
+    private int accountNumber;
+    private String accountHolderName;
+    private double balance;
+    private String email;
+    private String phoneNumber;
+
+    public Account(int accountNumber, String name, double balance,
+                   String email, String phoneNumber) {
+        this.accountNumber = accountNumber;
+        this.accountHolderName = name;
+        this.balance = balance;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public void deposit(double amount) {
+        if (amount > 0) {
+            balance += amount;
+            System.out.println("Amount deposited successfully!");
+        } else {
+            System.out.println("Invalid amount! Deposit must be positive.");
+        }
+    }
+
+    public void withdraw(double amount) {
+        if (amount <= 0) {
+            System.out.println("Invalid amount! Must be positive.");
+        } else if (amount > balance) {
+            System.out.println("Insufficient balance!");
+        } else {
+            balance -= amount;
+            System.out.println("Withdrawal successful!");
+        }
+    }
+
+    public void displayAccountDetails() {
+        System.out.println("Account Number : " + accountNumber);
+        System.out.println("Holder Name    : " + accountHolderName);
+        System.out.println("Balance        : " + balance);
+        System.out.println("Email          : " + email);
+        System.out.println("Phone Number   : " + phoneNumber);
+    }
+
+    public void updateContactDetails(String email, String phone) {
+        this.email = email;
+        this.phoneNumber = phone;
+        System.out.println("Contact details updated successfully!");
+    }
+
+    public int getAccountNumber() {
+        return accountNumber;
+    }
+}
+
+   
+ 
